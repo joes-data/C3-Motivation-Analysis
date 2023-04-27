d2279d9420ac8287d9643bc4de745ec76a2caf7d
Johannes Reithmeier <johannes.ch.reithmeier@gmail.com>
2023-04-27 17:15:57 +0200

    exclusion criteria

--- DataAnalysis_Expra_FSS2023_Group2_Motivation.R
+++ DataAnalysis_Expra_FSS2023_Group2_Motivation.R
@@ -14,26 +14,30 @@
 # Use RStudio menu: Session -> Set Working Directory -> To Source File Location
 
 # My working directory:
-setwd("C:/Users/Administrator/bwSyncShare/Lehre/Expra FSS2023/Group 2 - Motivation/Analyse")
+setwd("./data") #we use relational paths now, so everyone can work on it. Important everybody needs
+# a sub folder called data where the data is stored
 
 ### Read in data
 # Several options:
 # 1) Use point-and-click menu under Environment (Import Dataset -> From Text (base) - follow instructions in menu)
 # 2) Or use this piece of code (only works if data file is in same location as working directory)
 # Important: Check whether the name is correct!!
-data <- read.csv("PreliminaryData_Expra_Group2_Motivation_2023.csv", stringsAsFactors = FALSE)
+data <- read.csv("FinalData_Expra_Group2_Motivation_2023.csv", stringsAsFactors = FALSE)
+
+prelim <- read.csv("PreliminaryData_Expra_Group2_Motivation_2023.csv", stringsAsFactors = FALSE)
 
 ### Load helpful packages
 # Extend this list with whatever packages you need
 library(tidyverse)
+library(dplyr)
 
 ## How many participants in our data set?
-n_distinct(data$VPN) # 52 participants (on April 20)
+n_distinct(data$VPN) # 116 participants (on April 27)
 
 ## How many participants per condition?
 data %>% group_by(condition) %>% summarise(n = n_distinct(VPN))
 
-# 16 control, 20 economy, 16 environment (on April 20)
+# 40 control, 41 economy, 35 environment (on April 27)
 
 
 # 1) Check exclusion criteria ---------------------------------------------
@@ -43,7 +47,39 @@ table(data$quality)
 
 data_wo_quality <- data %>% filter(quality == "serious")
 
-## Exclusion: 
+## Exclusion: Manipulation check
+data_manipulated <- data_wo_quality %>% filter(Manipulation_Question =="yes")
+
+## Exclusion: Attention check
+# Subset for every condition and then for the correct answer scheme.
+environment_attention <- subset(data_manipulated, condition == "pro_environment") %>%
+  subset(AC_environment_true_preserve == TRUE & AC_environment_true_harmony == TRUE &
+           AC_environment_false_events == FALSE & AC_environment_false_neighbor == FALSE)
+
+
+economy_attention <- subset(data_manipulated, condition == "pro_economy") %>%
+  subset(AC_economy_false_international == FALSE & AC_economy_false_neighbor == FALSE &
+           AC_economy_true_crisis == TRUE & AC_economy_true_modern == TRUE)
+
+
+control_attention <- subset(data_manipulated, condition == "control") %>%
+  subset(AC_education_false_events == FALSE & AC_education_false_neighbor == FALSE &
+           AC_education_true_quality == TRUE & AC_education_true_access == TRUE)
+
+# merge datasets per condition to one big data frame
+included_data <- rbind(control_attention, economy_attention, environment_attention)
+
+## Exclusion: Did not take part in experiment
+
+## Final data summary
+# How many participants in our data set?
+n_distinct(included_data$VPN) # 76 participants (on April 27)
+
+# How many participants per condition?
+included_data %>% group_by(condition) %>% summarise(n = n_distinct(VPN))
+
+# 23 control, 29 economy, 24 environment (on April 27)
+
 
 # 2) Inspect socio-demographics -------------------------------------------
 
